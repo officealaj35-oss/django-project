b6925a4294fe64b5704334be876dc7c88bed28df
Ajay Saini <officealaj35@gmail.com>
2026-04-18 00:57:55 +0530

    fix image url

--- shop/urls.py
+++ shop/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path("product/<int:id>/", views.product_view, name='product_view'),
     path("checkout/" , views.checkout, name='checkout'),
     path("checkout-success/", views.checkout_success, name="checkout_success"),
-
-    # 🔥 Login system routes (NEW)
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
