86d9c22363eb21aaaf34f93b2cc5afee4845904f
Ajay Saini <officealaj35@gmail.com>
2026-04-18 11:43:39 +0530

    fix import path

--- mac/urls.py
+++ mac/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from shop.views import create_admin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/' , include('shop.urls')),
-    path('blog/' , include('blog.urls')),
+    path('shop/', include('shop.urls')),
+    path('blog/', include('blog.urls')),
+    path('create_admin/', create_admin),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
