051f8db93b22b3b434b8e75ef5e8c68a194e8cb2
Ajay Saini <officealaj35@gmail.com>
2026-04-23 11:11:12 +0530

    fix whitenoise static error

--- mac/settings.py
+++ mac/settings.py
@@ -134,8 +134,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 
