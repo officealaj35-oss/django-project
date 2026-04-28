0273e18a4dc94298b6ffea62f72b1078b6da0619
Ajay Saini <officealaj35@gmail.com>
2026-04-28 12:15:49 +0530

    fix logout url

--- mac/urls.py
+++ mac/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from shop.views import create_admin
 
@@ -15,6 +16,7 @@ urlpatterns = [
 
     # optional
     path('create_admin/', create_admin),
+path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
