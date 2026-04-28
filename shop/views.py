09b1b9bb30b7eb779ba954442facb63ba73185a8
Ajay Saini <officealaj35@gmail.com>
2026-04-28 12:44:21 +0530

    final logout fix

--- shop/views.py
+++ shop/views.py
@@ -230,3 +230,10 @@ def create_admin(request):
     user.save()
 
     return HttpResponse("Admin reset created")
+
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def user_logout(request):
+    logout(request)
+    return redirect('/shop/login/')
